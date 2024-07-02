6f94733d6059c2aee4a18a7b3b3705cae2194c0d
iwnelion <anname17@hotmail.com>
2024-07-02 12:28:10 +0300

    update user input

--- etch.js
+++ etch.js
@@ -1,16 +1,39 @@
 const grindContainer=document.createElement('div');
+grindContainer.id='grid';
 grindContainer.style.display='flex';
 grindContainer.style.flexWrap='wrap';
-grindContainer.style.justifyContent='pace-between';
 document.body.appendChild(grindContainer);
 
-for(let i=0;i<16;i++){
-    for(let j=0;j<16;j++){
-        const cell=document.createElement('div');
-        cell.style.width='50px';
-        cell.style.height='50px';
-        cell.style.backgroundColor='lightblue';
-        cell.style.border='1px solid black';
-        grindContainer.appendChild(cell);
+let initialGrid=16;
+
+let gridNumElement=document.getElementById('gridNum');
+gridNumElement.addEventListener('click', function(){
+    console.log("changing number of grid");
+    let newGrid=prompt("Enter a number of grids");
+    console.log("new grid number: "+newGrid);
+    if(newGrid>0 && newGrid<=100){
+        initialGrid=newGrid;
+    }else{
+        alert("Please enter a number between 1 and 100");
+    }
+});
+function createGrid(){
+    for(let i=0;i<initialGrid;i++){
+        for(let j=0;j<initialGrid;j++){
+            const cell=document.createElement('div');
+            cell.style.width='50px';
+            cell.style.height='50px';
+            cell.style.border='1px solid rgba(0, 0, 0, 0.5)';
+            grindContainer.appendChild(cell);
+        }
     }
 }
+
+function coloring(event){
+    console.log('coloring');
+    event.target.style.backgroundColor='powderblue';
+}
+
+grindContainer.addEventListener('mouseover', coloring);
+
+createGrid();

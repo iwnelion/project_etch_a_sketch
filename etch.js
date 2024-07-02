79001c6de6ec0dbaef41031569af6403fbb5e739
iwnelion <anname17@hotmail.com>
2024-07-02 14:33:39 +0300

    finish js

--- etch.js
+++ etch.js
@@ -1,10 +1,33 @@
-const grindContainer=document.createElement('div');
-grindContainer.id='grid';
-grindContainer.style.display='flex';
-grindContainer.style.flexWrap='wrap';
-document.body.appendChild(grindContainer);
+const gridContainer=document.createElement('div');
+gridContainer.id='grid';
+gridContainer.style.display='flex';
+gridContainer.style.flexWrap='wrap';
+document.body.appendChild(gridContainer);
 
 let initialGrid=16;
+
+function createGrid(){
+    gridContainer.innerHTML='';
+    for(let i=0;i<initialGrid;i++){
+        const row=document.createElement('div');
+        for(let j=0;j<initialGrid;j++){
+            const cell=document.createElement('div');
+            cell.style.width='50px';
+            cell.style.height='50px';
+            cell.style.border='1px solid rgba(0, 0, 0, 0.5)';
+            row.appendChild(cell);
+        }
+        gridContainer.appendChild(row);
+    }
+    gridContainer.addEventListener('mouseover', coloring);
+}
+
+function coloring(event){
+    console.log('coloring');
+    event.target.style.backgroundColor='powderblue';
+}
+
+createGrid();
 
 let gridNumElement=document.getElementById('gridNum');
 gridNumElement.addEventListener('click', function(){
@@ -13,27 +36,8 @@ gridNumElement.addEventListener('click', function(){
     console.log("new grid number: "+newGrid);
     if(newGrid>0 && newGrid<=100){
         initialGrid=newGrid;
+        createGrid();
     }else{
         alert("Please enter a number between 1 and 100");
     }
 });
-function createGrid(){
-    for(let i=0;i<initialGrid;i++){
-        for(let j=0;j<initialGrid;j++){
-            const cell=document.createElement('div');
-            cell.style.width='50px';
-            cell.style.height='50px';
-            cell.style.border='1px solid rgba(0, 0, 0, 0.5)';
-            grindContainer.appendChild(cell);
-        }
-    }
-}
-
-function coloring(event){
-    console.log('coloring');
-    event.target.style.backgroundColor='powderblue';
-}
-
-grindContainer.addEventListener('mouseover', coloring);
-
-createGrid();
